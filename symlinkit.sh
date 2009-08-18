ae9391f905c4e2f25e8d4d74e460e813a74d7bcc
Yann Thomas-Gerard <inside@gmail.com>
2009-08-18 16:11:19 +0200

    Symlinks every versionned dotfiles

--- symlinkit.sh
+++ symlinkit.sh
@@ -2,13 +2,27 @@
 #
 # "borrowed" from Jonathan Palardy (http://github.com/jpalardy/etc_config/tree/master)
  
+DOTFILES_DIR=~/github/dotfiles/
+
 function relink()
 {
-  rm $1
-  ln -s $2 $1
+    rm $1
+    ln -s $2 $1
 }
  
 cd
  
-relink .vim github/dotfiles/.vim
-relink .vimrc github/dotfiles/.vimrc
+for I in $(ls -a $DOTFILES_DIR)
+do
+    if [[ \
+        $I == '.' || \
+        $I == '..' || \
+        $I == '.git' || \
+        $I == '.gitignore' || \
+        $I == $(basename $0) ]]
+    then
+        continue
+    fi
+
+    relink $I "$DOTFILES_DIR$I"
+done
